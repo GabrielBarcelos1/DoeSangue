4e19798777e52e41016bb0879b071201551f5b4e
GabrielBarcelos1 <57267134+GabrielBarcelos1@users.noreply.github.com>
2020-02-25 21:04:38 -0300

    mudanças novas

--- server.js
+++ server.js
@@ -2,19 +2,28 @@
 const express = require("express")
 const server = express()
 
+//configurar o servidor para apresentar arquivos estaticos
+server.use(express.static('public'))
 
+
+
+//configurando a template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server
+})
 
 
 
 //configurar apresentação da pagina
 server.get("/", function(req, res){
-    return res.send("ok, cheguei aqui")
+    return res.render("index.html")
 })
 
 
 
 
-//ligar o servidor e perm
+//ligar o servidor e permitir o acesso na porta 3000
 server.listen(3000,function(){
     console.log("iniciei o servidor")
 })
